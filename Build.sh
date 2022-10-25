2e286ea31b736bec24c576db7734f2844b633683
jw007123 <ifitton845@gmail.com>
2022-10-26 00:21:30 +0100

    Linking against gl3w on Ubuntu

--- Build.sh
+++ Build.sh
@@ -1,14 +1,48 @@
 #!/bin/sh
-rm -rf External
 
-git clone https://gitlab.com/libeigen/eigen.git External/eigen
-git clone https://github.com/nothings/stb.git External/stb
-git clone https://github.com/ocornut/imgui.git External/imgui
-git clone https://github.com/epezent/implot.git External/implot
-git clone https://github.com/skaslev/gl3w.git External/gl3w
-git clone https://github.com/glfw/glfw.git External/glfw
+if [ ! -d "External/eigen" ]
+then
+    git clone https://gitlab.com/libeigen/eigen.git External/eigen
+fi
 
-mkdir Build
+if [ ! -d "External/stb" ]
+then
+    git clone https://github.com/nothings/stb.git External/stb
+fi
+
+if [ ! -d "External/imgui" ]
+then
+    git clone https://github.com/ocornut/imgui.git External/imgui
+fi
+
+if [ ! -d "External/implot" ]
+then
+    git clone https://github.com/epezent/implot.git External/implot
+fi
+
+if [ ! -d "External/gl3w" ]
+then
+    git clone https://github.com/skaslev/gl3w.git External/gl3w
+
+    cd External/gl3w
+    cmake .
+    make
+    cd ../..
+fi
+
+if [ ! -d "External/glfw" ]
+then
+    git clone https://github.com/glfw/glfw.git External/glfw
+
+
+
+fi
+
+if [ ! -d "Build" ]
+then
+    mkdir Build
+fi
+
 cd Build
 cmake ..
 make
--- Include/Utility/Literals.h
+++ Include/Utility/Literals.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstddef>
+
 typedef double                 f64;
 typedef float                  f32;
 typedef int                    i16;
