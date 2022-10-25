7e71307456e5f0bb6b37ece9e3a959a6d27479e0
ifitton <ifitton845@gmail.com>
2022-10-25 23:16:59 +0100

    Continue with setting up build system

--- Build.sh
+++ Build.sh
@@ -3,6 +3,10 @@ rm -rf External
 
 git clone https://gitlab.com/libeigen/eigen.git External/eigen
 git clone https://github.com/nothings/stb.git External/stb
+git clone https://github.com/ocornut/imgui.git External/imgui
+git clone https://github.com/epezent/implot.git External/implot
+git clone https://github.com/skaslev/gl3w.git External/gl3w
+git clone https://github.com/glfw/glfw.git External/glfw
 
 mkdir Build
 cd Build
